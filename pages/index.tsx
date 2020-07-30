f23b39ef518659f3d7ad6bb1525f45f591b0fc9b
santanarscs <raphaelstn@gmail.com>
2020-07-30 09:18:42 -0300

    feat: add formik and initial form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
+import { Formik, Form, Field } from 'formik'
+const Index = () => {
 
-const Index = () => <h1>Teste</h1>
+  return (
+    <Formik initialValues={{
+      director: '',
+      movieName: '',
+      yearReleased: ''
+    }}
+    onSubmit={(values) => console.log(values)}>
+      <Form>
+        <label>
+          Movie name
+          <Field name="movieName" type="text"></Field>
+        </label>
+        <label>
+          Director
+          <Field name="director" type="text"></Field>
+        </label>
+        <label>
+          Year Released
+          <Field name="yearReleased" type="text"></Field>
+        </label>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  )
+}
 
 export default Index;
